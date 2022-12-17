a22238c8f732e85d10a6017dd380b179a548131d
Vladimir NAFISSI <vnafissi@e3r8p9.clusters.42paris.fr>
2022-12-17 14:05:27 +0100

    ex00

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 
-int main(void) {
-	// char	buff[500];
-
-	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-
-
+char	ft_to_uppercase(int c) {
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
 }
 
+void	write_str_to_uppercase(char *s) {
+	int	i = 0;
+	if (!s)
+		return;
+	while (s[i]) {
+		std::cout << ft_to_uppercase(s[i]);
+		i++;
+	}
+}
+
+int main(int argc, char** argv) {
+	int	i = 1;
+	if (i < argc) {
+		while (i < argc) {
+			write_str_to_uppercase(argv[i]);
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+
+
+	return 0;
+}
+
+// int		ft_strlen(char *s) {
+// 	int	i = 0;
+// 	if (!s)
+// 		return (0);
+// 	while (s[i])
+// 		i++;
+// 	return (i);
+// }
