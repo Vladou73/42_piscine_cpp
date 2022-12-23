f1d4e57566718dc9d52ace18a2202c900c51043f
Vladimir Nafissi <vlad.nafissi@gmail.com>
2022-12-23 19:08:06 +0100

    ex02 started

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vladimir <vladimir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 15:00:14 by vnafissi          #+#    #+#             */
-/*   Updated: 2022/12/23 17:52:50 by vladimir         ###   ########.fr       */
+/*   Updated: 2022/12/23 19:07:45 by vladimir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,13 @@
 #include "Zombie.hpp"
 
 int main(void) {
-	int	N = 4;
-	
-	std::cout << "Creation d'une horde de zombies avec allocation dynamique" << std::endl;
-	Zombie* zombie_horde = zombieHorde(N, "Zozo le zombie");
-	
-	for (int i = 0; i < N ; i++) {
-		std::cout << "zombie n° " << i + 1 << std::endl;
-		zombie_horde[i].announce();
-	}
-	
-	std::cout << "delete la horde de zombies pour libérer la mémoire allouée" << std::endl;
-	delete [] zombie_horde;
-	
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string &stringREF = str;
+
+"• L’adresse de la string en mémoire.
+• L’adresse stockée dans stringPTR.
+• L’adresse stockée dans stringREF.
+"		
 	return 0;
 }
