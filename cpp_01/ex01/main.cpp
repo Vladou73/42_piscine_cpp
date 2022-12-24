f7fd4981dbb91f2126cd618136c08a79c707ec12
Vladimir Nafissi <vlad.nafissi@gmail.com>
2022-12-24 11:48:08 +0100

    cpp01 ex02 done

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -1,27 +1,20 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vladimir <vladimir@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/19 15:00:14 by vnafissi          #+#    #+#             */
-/*   Updated: 2022/12/23 19:07:45 by vladimir         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <iostream>
 #include <string>
 #include "Zombie.hpp"
 
 int main(void) {
-	std::string str = "HI THIS IS BRAIN";
-	std::string* stringPTR = &str;
-	std::string &stringREF = str;
-
-"• L’adresse de la string en mémoire.
-• L’adresse stockée dans stringPTR.
-• L’adresse stockée dans stringREF.
-"		
+	int	N = 4;
+	
+	std::cout << "Creation d'une horde de zombies avec allocation dynamique" << std::endl;
+	Zombie* zombie_horde = zombieHorde(N, "Zozo le zombie");
+	
+	for (int i = 0; i < N ; i++) {
+		std::cout << "zombie n° " << i + 1 << std::endl;
+		zombie_horde[i].announce();
+	}
+	
+	std::cout << "delete la horde de zombies pour libérer la mémoire allouée" << std::endl;
+	delete [] zombie_horde;
+	
 	return 0;
 }
--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -1,32 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vladimir <vladimir@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/19 15:00:14 by vnafissi          #+#    #+#             */
-/*   Updated: 2022/12/23 17:52:50 by vladimir         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <iostream>
 #include <string>
-#include "Zombie.hpp"
 
 int main(void) {
-	int	N = 4;
-	
-	std::cout << "Creation d'une horde de zombies avec allocation dynamique" << std::endl;
-	Zombie* zombie_horde = zombieHorde(N, "Zozo le zombie");
-	
-	for (int i = 0; i < N ; i++) {
-		std::cout << "zombie n° " << i + 1 << std::endl;
-		zombie_horde[i].announce();
-	}
-	
-	std::cout << "delete la horde de zombies pour libérer la mémoire allouée" << std::endl;
-	delete [] zombie_horde;
-	
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << &str << std::endl; //L’adresse de la string en mémoire
+	std::cout << stringPTR << std::endl; //"L’adresse stockée dans stringPTR
+	std::cout << &stringREF << std::endl; //L’adresse stockée dans stringREF
+
+	std::cout << str << std::endl; //La valeur de la string
+	std::cout << *stringPTR << std::endl; //La valeur pointée par stringPTR
+	std::cout << stringREF << std::endl; //La valeur pointée par stringREF
+
 	return 0;
 }
